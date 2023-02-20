19fa1808e6a4f9126d56997c13af8e3259bb013d
jharol <jharolrojas1@gmail.com>
2023-02-19 21:04:15 -0500

    volver a la configuracion anterior

--- routes/lead.route.js
+++ routes/lead.route.js
@@ -1,25 +1,29 @@
-const express = require("express");
-const { createLeadValidators } = require("../middlewares/validator.middleware");
-const { protectSession } = require("../middlewares/protectSession.middleware");
-const { protectOperator } = require("../middlewares/auth.middleware");
+const express = require('express');
+const {createLeadValidators} = require("../middlewares/validator.middleware")
+const {protectSession} = require("../middlewares/protectSession.middleware")
+const {protectOperator} = require("../middlewares/auth.middleware")
 
 // Controllers
 const {
-  getAllLead,
-  createLead,
-  updateLead,
-  statusLead,
-} = require("../controllers/lead.controller");
+    getAllLead,
+    createLead,
+    updateLead,
+    statusLead
+} = require('../controllers/lead.controller');
+
 
 const leadRouter = express.Router();
-leadRouter.use(protectSession);
 
-leadRouter.get("/", getAllLead);
+leadRouter.get('/', getAllLead);
 
-leadRouter.post("/", createLeadValidators, createLead);
+leadRouter.use(protectSession)
 
-leadRouter.patch("/:leadId", updateLead);
+leadRouter.post('/',  createLeadValidators,createLead);
 
-leadRouter.delete("/status/:leadId", statusLead);
+leadRouter.patch('/:leadId',  updateLead);
+
+leadRouter.delete('/status/:leadId',  statusLead);
+
+
 
 module.exports = { leadRouter };
